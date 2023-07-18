a615b73fa895069d8be16a6a054dca7c4983655c
oriavsapir <85383966+oriavsapir@users.noreply.github.com>
2023-07-18 07:39:34 +0300

    Update autonoip.py

--- autonoip.py
+++ autonoip.py
@@ -1,11 +1,11 @@
-import re
-import time
+from re import IGNORECASE, findall, match
+from time import sleep, time
 from requests import get
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import argparse
+from argparse import ArgumentParser
 
 
 def sign_in(username, password, show_browser):
@@ -22,7 +22,7 @@ def sign_in(username, password, show_browser):
 
     # Register user
     driver.get('https://www.noip.com/sign-up')  # Open the registration page
-    time.sleep(2)  # Wait for page to load
+    sleep(2)  # Wait for page to load
 
     # Fill in the registration form
     user = driver.find_element(By.ID, "email")
@@ -30,7 +30,7 @@ def sign_in(username, password, show_browser):
     password_input = driver.find_element(By.ID, "inputPassword")
     password_input.send_keys(password)
     driver.find_element(By.ID, "free-signup").click()
-    time.sleep(2)  # Wait for registration to complete
+    sleep(2)  # Wait for registration to complete
 
     # Close the WebDriver
     driver.quit()
@@ -42,7 +42,7 @@ def choose_domain(driver, hostname, ip_address):
     button = driver.find_element(By.XPATH, "//button[contains(text(), 'Create Hostname')]")
     # Click on the button
     button.click()
-    time.sleep(1)
+    sleep(1)
     # Fill in the form fields
     hostname_input = driver.find_element(By.ID, "name")
     ipv4_address_input = driver.find_element(By.NAME, "target")
@@ -50,13 +50,8 @@ def choose_domain(driver, hostname, ip_address):
     hostname_input.send_keys(hostname)
     ipv4_address_input.send_keys(ip_address)
     # TODO: fix choose domain.
-    # wait = WebDriverWait(driver, 10)
-    # dropdown = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "chosen-single")))
-    # dropdown.click()
-    # webdriver.ActionChains(driver).send_keys("ddnsking.com").perform()
-    # time.sleep(1)
     webdriver.ActionChains(driver).send_keys(Keys.ENTER).perform()
-    time.sleep(2)
+    sleep(2)
     element = driver.find_element(By.ID, "app")
     source = element.get_attribute("outerHTML")
     return source
@@ -83,7 +78,7 @@ def create_subdomain(email_input, password_input, hostname, ip_address, show_bro
 
     # Click on the login button
     driver.find_element(By.XPATH, "//button[contains(text(), 'Log In')]").click()
-    time.sleep(3)
+    sleep(3)
     webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
     # Wait for the dynamic DNS page to load
     source = choose_domain(driver, hostname, ip_address)
@@ -114,8 +109,8 @@ def generate_temporary_email():
 def get_activation_email(email):
     print("[*] Strating to active you mail account...")
     url = f'https://www.1secmail.com/api/v1/?action=getMessages&login={email.split("@")[0]}&domain={email.split("@")[1]}'
-    start_time = time.time()
-    while time.time() - start_time < 60:
+    start_time = time()
+    while time() - start_time < 60:
         response = get(url)
         data = response.json()
 
@@ -126,7 +121,7 @@ def get_activation_email(email):
                 break
 
         print("[*] Still there is no response yet. Waiting for", 3, "seconds...")
-        time.sleep(3)
+        sleep(3)
 
     if not data:
         print("No response received within the specified timeout.")
@@ -135,7 +130,7 @@ def get_activation_email(email):
     email_url = f'https://www.1secmail.com/api/v1/?action=readMessage&login={email.split("@")[0]}&domain={email.split("@")[1]}&id={email_id}'
     response = get(email_url)
     messages = response.json()
-    link_matches = re.findall(r'<a\s+href="(.*?)"', messages['body'], re.IGNORECASE)
+    link_matches = findall(r'<a\s+href="(.*?)"', messages['body'], IGNORECASE)
 
     if len(link_matches) >= 2:
         check_activation = get(link_matches[1]).text
@@ -147,7 +142,7 @@ def get_activation_email(email):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = ArgumentParser()
     parser.add_argument("-p", "--password", help="password (at least 6 characters)")
     parser.add_argument("--IP", help="IP address")
     parser.add_argument("-sb", "--subdomain", help="subdomain name (up to 19 characters). "
@@ -165,9 +160,9 @@ if __name__ == '__main__':
 
     # Validate the IP address
     ipv4_pattern = r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
-    if not args.IP or not re.match(ipv4_pattern, args.IP):
+    if not args.IP or not match(ipv4_pattern, args.IP):
         args.IP = input("[*] Insert IP address: \n")
-        while not re.match(ipv4_pattern, args.IP):
+        while not match(ipv4_pattern, args.IP):
             print("Please enter a valid IPv4 address.")
             args.IP = input("[*] Insert IP address: \n")
 
